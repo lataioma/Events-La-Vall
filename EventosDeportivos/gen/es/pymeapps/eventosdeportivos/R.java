6414f36ab32a96440237a0fc2a04478df069593b
appla_000 <appla_000@192.168.1.40>
2014-07-16 16:07:16 +0200

    creacion layouts de la activity principal

--- EventosDeportivos/gen/es/pymeapps/eventosdeportivos/R.java
+++ EventosDeportivos/gen/es/pymeapps/eventosdeportivos/R.java
@@ -10,33 +10,52 @@ package es.pymeapps.eventosdeportivos;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blanco=0x7f040001;
+        public static final int rosa=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
-
-         Example customization of dimensions originally defined in res/values/dimens.xml
-         (such as screen margins) for screens with more than 820dp of available width. This
-         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
-    
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int letra_mediana=0x7f050002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int img_assyclubs=0x7f020001;
+        public static final int img_deportes=0x7f020002;
+        public static final int img_eventos=0x7f020003;
+        public static final int img_informacion=0x7f020004;
+        public static final int img_instalaciones=0x7f020005;
+        public static final int img_noticias=0x7f020006;
+        public static final int img_servicios=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LinearLayout1=0x7f090000;
+        public static final int action_settings=0x7f090008;
+        public static final int imageView1=0x7f090001;
+        public static final int imageView2=0x7f090002;
+        public static final int imageView3=0x7f090004;
+        public static final int imageView4=0x7f090006;
+        public static final int textView1=0x7f090003;
+        public static final int textView2=0x7f090005;
+        public static final int textView3=0x7f090007;
     }
     public static final class layout {
         public static final int activity_inicio=0x7f030000;
     }
     public static final class menu {
-        public static final int inicio=0x7f070000;
+        public static final int inicio=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int txtbtn_inici_eventos=0x7f060004;
+        public static final int txtbtn_inici_informacion=0x7f060003;
+        public static final int txtbtn_inici_noticias=0x7f060005;
+        public static final int txtbtn_inici_servicios=0x7f060006;
     }
     public static final class style {
         /** 
@@ -60,10 +79,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
